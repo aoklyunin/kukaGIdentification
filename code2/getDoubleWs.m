318a0f78e268b308359c34f52eceef0deaaa1252
aoklyunin <aoklyunin@gmail.com>
2018-01-24 16:47:07 +0300

    kk

--- code2/getDoubleWs.m
+++ code2/getDoubleWs.m
@@ -5,7 +5,7 @@ load('../workspace/Ws.mat')
 s = size(T);
 WS=[]; 
 for i=1:1:s(1)   
-   Wm = double(subs(Ws,[q1 q2 q3 q4 q5],[J(i,1)  J(i,2)  J(i,3)  J(i,4) 0]));
+   Wm = double(subs(Ws,[q1 q2 q3 q4 q5],[J(i,1)  J(i,2)  J(i,3)  J(i,4) J(i,5)]));
    WS(i,:,:) = [Wm;diag(multiSign(T(i,:)'))];
 end
 
